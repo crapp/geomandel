2e01040af2ca64702cd9fb32a796389278e58687
Christian Rapp <0x2a@posteo.org>
2016-02-29 11:45:19 +0100

    Fix commandline unit tests
    I rewrote the unit tests for the command line parser. Actually I have no
    idea why I had problems with them on osx but I hope they are fixed now.
    
    Upgraded cxxopts header file to latest version
    
    Additional build for travis ci with a more recent osx_image

--- src/test/test_commandline.cpp
+++ src/test/test_commandline.cpp
@@ -30,175 +30,144 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 cxxopts::Options generate_empty_parser()
 {
-    cxxopts::Options parser("UnitTest command line parser");
+    cxxopts::Options parser("Unittester", "Geomandel unittester");
     configure_command_line_parser(parser);
     return parser;
 }
 
 TEST_CASE("Command line arguments", "[commandline]")
 {
-    std::vector<const char *> test_argv_vec;
-    //const char *test_argv[] = {""};
     SECTION("Basic parser checking, expecting exception")
     {
         auto parser = generate_empty_parser();
-        const char *test_argv[] = {"some", "idiotic", "--options", NULL};
-        char **blub = const_cast<char **>(test_argv);
+        const char *test_argv[] = {"some", "idiotic", "--options"};
+        char **cxxopt_pointer = const_cast<char **>(test_argv);
         int test_argc = 3;
-        REQUIRE_THROWS(parser.parse(test_argc, blub));
+        REQUIRE_THROWS(parser.parse(test_argc, cxxopt_pointer));
     }
-    //SECTION("Test options that need an argument")
-    //{
-        //// bailout
-        //auto parser = generate_empty_parser();
-        //test_argv_vec.clear();
-        //test_argv_vec = {"Unittester", "-b"};
-        //test_argv = const_cast<char **>(test_argv_vec.data());
-        //test_argc = static_cast<int>(test_argv_vec.size());
-        //REQUIRE_THROWS(parser.parse(test_argc, test_argv));
-        //// creal-min
-        //parser = generate_empty_parser();
-        //test_argv_vec.clear();
-        //test_argv_vec = {"Unittester", "--creal-min"};
-        //test_argv = const_cast<char **>(test_argv_vec.data());
-        //test_argc = static_cast<int>(test_argv_vec.size());
-        //REQUIRE_THROWS(parser.parse(test_argc, test_argv));
-        //// creal-max
-        //parser = generate_empty_parser();
-        //test_argv_vec.clear();
-        //test_argv_vec = {"Unittester", "--creal-max"};
-        //test_argv = const_cast<char **>(test_argv_vec.data());
-        //test_argc = static_cast<int>(test_argv_vec.size());
-        //REQUIRE_THROWS(parser.parse(test_argc, test_argv));
-        //// cima-min
-        //parser = generate_empty_parser();
-        //test_argv_vec.clear();
-        //test_argv_vec = {"Unittester", "--cima-min"};
-        //test_argv = const_cast<char **>(test_argv_vec.data());
-        //test_argc = static_cast<int>(test_argv_vec.size());
-        //REQUIRE_THROWS(parser.parse(test_argc, test_argv));
-        //// cima-max
-        //parser = generate_empty_parser();
-        //test_argv_vec.clear();
-        //test_argv_vec = {"Unittester", "--cima-max"};
-        //test_argv = const_cast<char **>(test_argv_vec.data());
-        //test_argc = static_cast<int>(test_argv_vec.size());
-        //REQUIRE_THROWS(parser.parse(test_argc, test_argv));
-        //// image-file
-        //parser = generate_empty_parser();
-        //test_argv_vec.clear();
-        //test_argv_vec = {"Unittester", "--image-file"};
-        //test_argv = const_cast<char **>(test_argv_vec.data());
-        //test_argc = static_cast<int>(test_argv_vec.size());
-        //REQUIRE_THROWS(parser.parse(test_argc, test_argv));
-        //// width
-        //parser = generate_empty_parser();
-        //test_argv_vec.clear();
-        //test_argv_vec = {"Unittester", "-w"};
-        //test_argv = const_cast<char **>(test_argv_vec.data());
-        //test_argc = static_cast<int>(test_argv_vec.size());
-        //REQUIRE_THROWS(parser.parse(test_argc, test_argv));
-        //// height
-        //parser = generate_empty_parser();
-        //test_argv_vec.clear();
-        //test_argv_vec = {"Unittester", "-h"};
-        //test_argv = const_cast<char **>(test_argv_vec.data());
-        //test_argc = static_cast<int>(test_argv_vec.size());
-        //REQUIRE_THROWS(parser.parse(test_argc, test_argv));
-        //// colalgo
-        //parser = generate_empty_parser();
-        //test_argv_vec.clear();
-        //test_argv_vec = {"Unittester", "--colalgo"};
-        //test_argv = const_cast<char **>(test_argv_vec.data());
-        //test_argc = static_cast<int>(test_argv_vec.size());
-        //REQUIRE_THROWS(parser.parse(test_argc, test_argv));
-        //// grey-base
-        //parser = generate_empty_parser();
-        //test_argv_vec.clear();
-        //test_argv_vec = {"Unittester", "--grey-base"};
-        //test_argv = const_cast<char **>(test_argv_vec.data());
-        //test_argc = static_cast<int>(test_argv_vec.size());
-        //REQUIRE_THROWS(parser.parse(test_argc, test_argv));
-        //// grey-freq
-        //parser = generate_empty_parser();
-        //test_argv_vec.clear();
-        //test_argv_vec = {"Unittester", "--grey-freq"};
-        //test_argv = const_cast<char **>(test_argv_vec.data());
-        //test_argc = static_cast<int>(test_argv_vec.size());
-        //REQUIRE_THROWS(parser.parse(test_argc, test_argv));
-        //// rgb-base
-        //parser = generate_empty_parser();
-        //test_argv_vec.clear();
-        //test_argv_vec = {"Unittester", "--rgb-base"};
-        //test_argv = const_cast<char **>(test_argv_vec.data());
-        //test_argc = static_cast<int>(test_argv_vec.size());
-        //REQUIRE_THROWS(parser.parse(test_argc, test_argv));
-        //// rgb-freq
-        //parser = generate_empty_parser();
-        //test_argv_vec.clear();
-        //test_argv_vec = {"Unittester", "--rgb-freq"};
-        //test_argv = const_cast<char **>(test_argv_vec.data());
-        //test_argc = static_cast<int>(test_argv_vec.size());
-        //REQUIRE_THROWS(parser.parse(test_argc, test_argv));
-        //// rgb-phase
-        //parser = generate_empty_parser();
-        //test_argv_vec.clear();
-        //test_argv_vec = {"Unittester", "--rgb-phase"};
-        //test_argv = const_cast<char **>(test_argv_vec.data());
-        //test_argc = static_cast<int>(test_argv_vec.size());
-        //REQUIRE_THROWS(parser.parse(test_argc, test_argv));
-        //// zoom
-        //parser = generate_empty_parser();
-        //test_argv_vec.clear();
-        //test_argv_vec = {"Unittester", "--zoom"};
-        //test_argv = const_cast<char **>(test_argv_vec.data());
-        //test_argc = static_cast<int>(test_argv_vec.size());
-        //REQUIRE_THROWS(parser.parse(test_argc, test_argv));
-        //// xcoord
-        //parser = generate_empty_parser();
-        //test_argv_vec.clear();
-        //test_argv_vec = {"Unittester", "--xcoord"};
-        //test_argv = const_cast<char **>(test_argv_vec.data());
-        //test_argc = static_cast<int>(test_argv_vec.size());
-        //REQUIRE_THROWS(parser.parse(test_argc, test_argv));
-        //// ycoord
-        //parser = generate_empty_parser();
-        //test_argv_vec.clear();
-        //test_argv_vec = {"Unittester", "--ycoord"};
-        //test_argv = const_cast<char **>(test_argv_vec.data());
-        //test_argc = static_cast<int>(test_argv_vec.size());
-        //REQUIRE_THROWS(parser.parse(test_argc, test_argv));
-    //}
-    //SECTION("Test command line parameters \"logic\"")
-    //{
-        //// max lower min not allowed
-        //std::shared_ptr<MandelParameters> params = nullptr;
-        //auto parser = generate_empty_parser();
-        //test_argv_vec.clear();
-        //test_argv_vec = {"Unittester", "--creal-min=-1.0", "--creal-max=-2.0"};
-        //test_argv = const_cast<char **>(test_argv_vec.data());
-        //test_argc = static_cast<int>(test_argv_vec.size());
-        //parser.parse(test_argc, test_argv);
-        //init_mandel_parameters(params, parser);
-        //REQUIRE(params == nullptr);
-        //params = nullptr;
-        //parser = generate_empty_parser();
-        //test_argv_vec.clear();
-        //test_argv_vec = {"Unittester", "--cima-min=5.2", "--cima-max=1.21"};
-        //test_argv = const_cast<char **>(test_argv_vec.data());
-        //test_argc = static_cast<int>(test_argv_vec.size());
-        //parser.parse(test_argc, test_argv);
-        //init_mandel_parameters(params, parser);
-        //REQUIRE(params == nullptr);
-        //// no parameters at all should give as a default Mandelparameters object
-        //params = nullptr;
-        //parser = generate_empty_parser();
-        //test_argv_vec.clear();
-        //test_argv_vec = {"Unittester"};
-        //test_argv = const_cast<char **>(test_argv_vec.data());
-        //test_argc = static_cast<int>(test_argv_vec.size());
-        //parser.parse(test_argc, test_argv);
-        //init_mandel_parameters(params, parser);
-        //REQUIRE(params != nullptr);
-    //}
+
+    SECTION("Test options that need an argument")
+    {
+        char **cxxopt_pointer = nullptr;
+        // bailout
+        auto parser = generate_empty_parser();
+        const char *test_argv_bail[] = {"Unittester", "-b"};
+        cxxopt_pointer = const_cast<char **>(test_argv_bail);
+        int test_argc = 2;
+        REQUIRE_THROWS(parser.parse(test_argc, cxxopt_pointer));
+        // creal-min
+        parser = generate_empty_parser();
+        const char *test_argv_crealm[] = {"Unittester", "--creal-min"};
+        cxxopt_pointer = const_cast<char **>(test_argv_crealm);
+        REQUIRE_THROWS(parser.parse(test_argc, cxxopt_pointer));
+        // creal-max
+        parser = generate_empty_parser();
+        const char *test_argv_crealmax[] = {"Unittester", "--creal-max"};
+        cxxopt_pointer = const_cast<char **>(test_argv_crealmax);
+        REQUIRE_THROWS(parser.parse(test_argc, cxxopt_pointer));
+        // cima-min
+        parser = generate_empty_parser();
+        const char *test_argv_cima_min[] = {"Unittester", "--cima-min"};
+        cxxopt_pointer = const_cast<char **>(test_argv_cima_min);
+        REQUIRE_THROWS(parser.parse(test_argc, cxxopt_pointer));
+        // cima-max
+        parser = generate_empty_parser();
+        const char *test_argv_cima_max[] = {"Unittester", "--cima-max"};
+        cxxopt_pointer = const_cast<char **>(test_argv_cima_max);
+        REQUIRE_THROWS(parser.parse(test_argc, cxxopt_pointer));
+        // image-file
+        parser = generate_empty_parser();
+        const char *test_argv_img_file[] = {"Unittester", "--image-file"};
+        cxxopt_pointer = const_cast<char **>(test_argv_img_file);
+        REQUIRE_THROWS(parser.parse(test_argc, cxxopt_pointer));
+        // width
+        parser = generate_empty_parser();
+        const char *test_argv_width[] = {"Unittester", "-w"};
+        cxxopt_pointer = const_cast<char **>(test_argv_width);
+        REQUIRE_THROWS(parser.parse(test_argc, cxxopt_pointer));
+        // height
+        parser = generate_empty_parser();
+        const char *test_argv_height[] = {"Unittester", "-h"};
+        cxxopt_pointer = const_cast<char **>(test_argv_height);
+        REQUIRE_THROWS(parser.parse(test_argc, cxxopt_pointer));
+        // colalgo
+        parser = generate_empty_parser();
+        const char *test_argv_colalgo[] = {"Unittester", "--colalgo"};
+        cxxopt_pointer = const_cast<char **>(test_argv_colalgo);
+        REQUIRE_THROWS(parser.parse(test_argc, cxxopt_pointer));
+        // grey-base
+        parser = generate_empty_parser();
+        const char *test_argv_grey_base[] = {"Unittester", "--grey-base"};
+        cxxopt_pointer = const_cast<char **>(test_argv_grey_base);
+        REQUIRE_THROWS(parser.parse(test_argc, cxxopt_pointer));
+        // grey-freq
+        parser = generate_empty_parser();
+        const char *test_argv_grey_freq[] = {"Unittester", "--grey-freq"};
+        cxxopt_pointer = const_cast<char **>(test_argv_grey_freq);
+        REQUIRE_THROWS(parser.parse(test_argc, cxxopt_pointer));
+        // rgb-base
+        parser = generate_empty_parser();
+        const char *test_argv_rgb_base[] = {"Unittester", "--rgb-base"};
+        cxxopt_pointer = const_cast<char **>(test_argv_grey_freq);
+        REQUIRE_THROWS(parser.parse(test_argc, cxxopt_pointer));
+        // rgb-freq
+        parser = generate_empty_parser();
+        const char *test_argv_rgb_freq[] = {"Unittester", "--rgb-freq"};
+        cxxopt_pointer = const_cast<char **>(test_argv_grey_freq);
+        REQUIRE_THROWS(parser.parse(test_argc, cxxopt_pointer));
+        // rgb-phase
+        parser = generate_empty_parser();
+        const char *test_argv_rgb_phase[] = {"Unittester", "--rgb-phase"};
+        cxxopt_pointer = const_cast<char **>(test_argv_rgb_phase);
+        REQUIRE_THROWS(parser.parse(test_argc, cxxopt_pointer));
+        // zoom
+        parser = generate_empty_parser();
+        const char *test_argv_zoom[] = {"Unittester", "--zoom"};
+        cxxopt_pointer = const_cast<char **>(test_argv_zoom);
+        REQUIRE_THROWS(parser.parse(test_argc, cxxopt_pointer));
+        // xcoord
+        parser = generate_empty_parser();
+        const char *test_argv_xcoord[] = {"Unittester", "--xcoord"};
+        cxxopt_pointer = const_cast<char **>(test_argv_xcoord);
+        REQUIRE_THROWS(parser.parse(test_argc, cxxopt_pointer));
+        // ycoord
+        parser = generate_empty_parser();
+        const char *test_argv_ycoord[] = {"Unittester", "--ycoord"};
+        cxxopt_pointer = const_cast<char **>(test_argv_ycoord);
+        REQUIRE_THROWS(parser.parse(test_argc, cxxopt_pointer));
+    }
+
+    SECTION("Test command line parameters \"logic\"")
+    {
+        // max lower min not allowed
+        std::shared_ptr<MandelParameters> params = nullptr;
+        auto parser = generate_empty_parser();
+        const char *test_argv_max_low_min[] = {"Unittester", "--creal-min=-1.8",
+                                               "--creal-max=-2.9"};
+        int test_argc = 3;
+        char **cxxopt_pointer = const_cast<char **>(test_argv_max_low_min);
+        parser.parse(test_argc, cxxopt_pointer);
+        init_mandel_parameters(params, parser);
+        REQUIRE(params == nullptr);
+
+        params = nullptr;
+        parser = generate_empty_parser();
+        const char *test_argv_max_low_min_ima[] = {"Unittester", "--cima-min=3",
+                                                   "--cima-max=1"};
+        cxxopt_pointer = const_cast<char **>(test_argv_max_low_min_ima);
+        test_argc = 3;
+        parser.parse(test_argc, cxxopt_pointer);
+        init_mandel_parameters(params, parser);
+        REQUIRE(params == nullptr);
+
+        // no parameters at all should give as a default Mandelparameters object
+        params = nullptr;
+        parser = generate_empty_parser();
+        const char *test_argv_empty[] = {"Unittester"};
+        test_argc = 1;
+        cxxopt_pointer = const_cast<char **>(test_argv_empty);
+        parser.parse(test_argc, cxxopt_pointer);
+        init_mandel_parameters(params, parser);
+        REQUIRE(params != nullptr);
+    }
 }
