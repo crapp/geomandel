ec3f33974368c6c16bb1cfac4fae723ec0d5ad13
Christian Rapp <0x2a@posteo.org>
2016-03-24 13:59:44 +0100

    Fix output tests
    Output unit tests were no longer working because we remove trailing
    zeros in the application now. Added primitive_to_string function that
    does the same for the unit tests.

--- src/test/test_output.cpp
+++ src/test/test_output.cpp
@@ -20,6 +20,35 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "buffwriter_mock.h"
 
+#include <type_traits>
+
+/**
+ * @brief Primitives to string conversion
+ *
+ * @tparam T Type of primitive to convert
+ * @param value
+ *
+ * @return String
+ *
+ * @details
+ * Converts primitive data types to strings using std::to_string. Additionally
+ * trailing '0' will be deleted.
+ */
+template <typename T>
+std::string primitive_to_string(T value)
+{
+    std::string val_to_string = std::to_string(value);
+    if (std::is_same<double, T>::value || std::is_same<float, T>::value) {
+        val_to_string.erase(val_to_string.find_last_not_of('0') + 1,
+                            std::string::npos);
+        // check if last char is a point
+        if (val_to_string.back() == '.') {
+            val_to_string = val_to_string.substr(0, val_to_string.size() - 1);
+        }
+    }
+    return val_to_string;
+}
+
 TEST_CASE("Filename Patterns", "[output]")
 {
     // some sane values for supported filename patterns
@@ -28,8 +57,8 @@ TEST_CASE("Filename Patterns", "[output]")
     unsigned int yrange = 768;    // %h
     unsigned int zoom = 0;        // %z
     unsigned int cores = 4;       // %c
-    unsigned int xcoord = 323;    // %x
-    unsigned int ycoord = 233;    // %y
+    double xcoord = 323;          // %x
+    double ycoord = 233;          // %y
     double z_real_min = -2.5;     // %Zr
     double z_real_max = 1.0;      // %ZR
     double z_ima_min = -1.5;      // %Zi
@@ -45,11 +74,23 @@ TEST_CASE("Filename Patterns", "[output]")
             pattern, bailout, xrange, yrange, zoom, cores, xcoord, ycoord,
             z_real_min, z_real_max, z_ima_min, z_ima_max);
         REQUIRE(filename ==
-                "mandi_" + std::to_string(xrange) + "x" +
-                    std::to_string(yrange) + "_" + std::to_string(zoom) +
-                    ".png");
+                "mandi_" + primitive_to_string(xrange) + "x" +
+                    primitive_to_string(yrange) + "_" +
+                    primitive_to_string(zoom) + ".png");
     }
 
+    SECTION("Special case where .0 floats should be displayed as integers")
+    {
+        std::string pattern = "mandeltest_[%x-%y]";
+        xcoord = 280.0;
+        ycoord = 178.0;
+        std::string filename = bmock.test_filename_patterns(
+            std::move(pattern), bailout, xrange, yrange, zoom, cores, xcoord,
+            ycoord, z_real_min, z_real_max, z_ima_min, z_ima_max);
+        REQUIRE(filename ==
+                "mandeltest_[" + primitive_to_string(xcoord) + "-" +
+                    primitive_to_string(ycoord) + "]");
+    }
     SECTION("Complex file name with all patterns")
     {
         // %a is a fake pattern
@@ -58,14 +99,17 @@ TEST_CASE("Filename Patterns", "[output]")
         std::string filename = bmock.test_filename_patterns(
             std::move(pattern), bailout, xrange, yrange, zoom, cores, xcoord,
             ycoord, z_real_min, z_real_max, z_ima_min, z_ima_max);
-        REQUIRE(
-            filename ==
-            "mandeltest_%a_" + std::to_string(xrange) + "x" +
-                std::to_string(yrange) + "_b" + std::to_string(bailout) + "_m" +
-                std::to_string(cores) + "_[" + std::to_string(xcoord) + ", " +
-                std::to_string(ycoord) + " --> " + std::to_string(zoom) + "]_(" +
-                std::to_string(z_real_min) + ", " + std::to_string(z_ima_min) +
-                ")->(" + std::to_string(z_real_max) + ", " +
-                std::to_string(z_ima_max) + ")");
+        REQUIRE(filename ==
+                "mandeltest_%a_" + primitive_to_string(xrange) + "x" +
+                    primitive_to_string(yrange) + "_b" +
+                    primitive_to_string(bailout) + "_m" +
+                    primitive_to_string(cores) + "_[" +
+                    primitive_to_string(xcoord) + ", " +
+                    primitive_to_string(ycoord) + " --> " +
+                    primitive_to_string(zoom) + "]_(" +
+                    primitive_to_string(z_real_min) + ", " +
+                    primitive_to_string(z_ima_min) + ")->(" +
+                    primitive_to_string(z_real_max) + ", " +
+                    primitive_to_string(z_ima_max) + ")");
     }
 }
